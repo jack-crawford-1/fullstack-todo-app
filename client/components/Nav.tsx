47adeb9516140b7a1e00aee7e8d17062c4b7fd37
Jack Crawford <primes_unit.05@icloud.com>
2024-03-25 15:33:38 +1300

    fixed button not displaying

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,5 +1,5 @@
 import { IfAuthenticated, IfNotAuthenticated } from './Auth.tsx'
-import { NavGroup, NavButton } from '../components/styling.tsx'
+import { NavGroup, NavButton } from '../components/Styling.tsx'
 import { useAuth0 } from '@auth0/auth0-react'
 
 function Nav() {
--- client/components/Styling.tsx
+++ client/components/Styling.tsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 export const NavGroup = styled.nav`
   float: right;
-  background-color: green;
 `
 
 export const NavLink = styled(Link)`
   margin-right: 30px;
 `
+export const NavButton = styled.button``
